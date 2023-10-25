1cf23736e9115d2c2097e9351017fa6b1ec58743
amolkapadi <amolakapadi@gmail.com>
2023-10-25 19:37:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+// import MixedChart from './Component/MixedChart';
+// import DoughnutChart from './Component/DoughnutChart';
+// import LilneChart from './Component/LilneChart';
+import PieChart from './Component/PieChart';
+// import BarChart from './Component/BarChart';
+// import BubbleChart from './Component/BubbleChart';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PieChart />
     </div>
   );
 }
